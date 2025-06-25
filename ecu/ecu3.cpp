88243b68d8aa9fed29ad4e1cef06892ff966f820
Krishneel Sahdeo <krishneel_sahdeo@yahoo.com>
2025-06-24 18:50:36 -0700

    Update ecu3.cpp

--- ecu/ecu3.cpp
+++ ecu/ecu3.cpp
@@ -1,1 +1,49 @@
 
+// ECU 3: Diagnostic ECU
+#include <SPI.h>
+#include <Ethernet.h>
+#include <mcp_can.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 177);       // Static IP of this ECU
+IPAddress server(192, 168, 1, 100);   // Your PC IP
+
+EthernetClient client;
+MCP_CAN CAN(10);
+
+void setup() {
+  Serial.begin(115200);
+  Ethernet.begin(mac, ip);
+  while (CAN_OK != CAN.begin(CAN_500KBPS)) {
+    Serial.println("CAN BUS Init Failed");
+    delay(100);
+  }
+  Serial.println("ECU 3 Ready");
+}
+
+void loop() {
+  // TCP heartbeat
+  if (client.connect(server, 5000)) {
+    client.println("Diagnostic ECU 3: Online");
+    client.stop();
+  }
+
+  // Log all CAN traffic
+  byte len = 0;
+  byte buf[8];
+  if (CAN_MSGAVAIL == CAN.checkReceive()) {
+    CAN.readMsgBuf(&len, buf);
+    unsigned long canId = CAN.getCanId();
+
+    Serial.print("CAN ID: 0x");
+    Serial.print(canId, HEX);
+    Serial.print(" Data: ");
+    for (int i = 0; i < len; i++) {
+      Serial.print(buf[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+
+  delay(5000);  // send heartbeat every 5s
+}
